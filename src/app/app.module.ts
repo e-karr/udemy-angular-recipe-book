afdac99cde144d2d9afe30125d8e69523663db8d
Elizabeth Karr <e.karr93@icloud.com>
2023-06-12 16:25:31 -0500

    generate recipe list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ShoppingListComponent
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipeListComponent
   ],
   imports: [
     BrowserModule,
