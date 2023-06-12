ddbba31fb7606cc834993fb07d8990a3ecba212e
Elizabeth Karr <e.karr93@icloud.com>
2023-06-12 16:36:18 -0500

    generate recipe-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeItemComponent } from './recipes/recipe-item/recipe-item.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { HeaderComponent } from './header/header.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { HeaderComponent } from './header/header.component';
     RecipeItemComponent,
     RecipeDetailComponent,
     HeaderComponent,
+    RecipeListComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
