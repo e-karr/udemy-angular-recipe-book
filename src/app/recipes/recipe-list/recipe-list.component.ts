a22f5020805f21fb8e970d89336d73f5485b1831
Elizabeth Karr <e.karr93@icloud.com>
2023-06-13 18:45:10 -0500

    add second test recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -13,6 +13,11 @@ export class RecipeListComponent implements OnInit {
       'this is a test',
       'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'
     ),
+    new Recipe(
+      'Test Recipe',
+      'this is a test',
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'
+    ),
   ];
 
   constructor() {}
