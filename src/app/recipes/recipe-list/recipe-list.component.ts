c060c5d7b2339cdf1fb046584043da583b2940dd
Elizabeth Karr <e.karr93@icloud.com>
2023-06-13 18:28:53 -0500

    create test Recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -4,16 +4,18 @@ import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-
-  recipes: Recipe[] = [];
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test Recipe',
+      'this is a test',
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'
+    ),
+  ];
 
   constructor() {}
 
-  ngOnInit(): void {
-    
-  }
-
+  ngOnInit(): void {}
 }
