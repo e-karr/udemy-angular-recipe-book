76dc987c281857b7f84d0225e4771533ffdb93dc
Elizabeth Karr <e.karr93@icloud.com>
2023-07-10 14:28:36 -0500

    code dropdown directive

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,10 +1,24 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
 })
 export class DropdownDirective {
+  @HostBinding('class.open') openClass: boolean = false;
 
-  constructor() { }
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
 
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    // if (this.openClass) {
+    //   this.renderer.removeClass(this.elementRef.nativeElement, 'open');
+    //   this.openClass = false;
+    // } else {
+    //   this.renderer.addClass(this.elementRef.nativeElement, 'open');
+    //   this.openClass = true;
+    // }
+
+    // this.openClass = !this.openClass;
+
+    this.openClass = this.elementRef.nativeElement.contains(event.target) ? !this.openClass : false;
+  }
 }
