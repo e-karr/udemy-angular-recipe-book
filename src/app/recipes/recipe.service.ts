5bac5ce3468eefda3ea6a16fd17ed22368588a84
Elizabeth Karr <e.karr93@icloud.com>
2023-07-11 10:25:07 -0500

    add ingredients to recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/Ingredient.model';
 
 export class RecipeService {
   //Manage recipes
@@ -7,14 +8,26 @@ export class RecipeService {
 
   private recipes: Recipe[] = [
     new Recipe(
-      'Test Recipe',
-      'this is a test',
-      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'
+      'Mac & Cheese',
+      'Creamy and cheese mac and cheese.',
+      'https://upload.wikimedia.org/wikipedia/commons/8/8f/Macaroni_and_cheese_%282%29_%28cropped%29.jpg',
+      [
+        new Ingredient('16 oz elbow noodles', 1),
+        new Ingredient('8 oz cheddar cheese', 1),
+        new Ingredient('8 oz heavy cream', 1),
+        new Ingredient('tablespoon butter', 2),
+      ]
     ),
     new Recipe(
-      'Another Test Recipe',
-      'this is a test',
-      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'
+      'Angus Burger',
+      'Quarter pound Angus beef',
+      'https://cdn.pixabay.com/photo/2019/01/21/12/47/burger-3946012_1280.jpg',
+      [
+        new Ingredient('1/4 lb Angus patty', 1),
+        new Ingredient('hamburger bun', 1),
+        new Ingredient('tomato slice', 1),
+        new Ingredient('lettuce leaf', 1),
+      ]
     ),
   ];
 
