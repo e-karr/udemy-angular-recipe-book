1a42159c821836dfae3cf89827496ba5f72f133f
Elizabeth Karr <e.karr93@icloud.com>
2023-08-07 15:00:13 -0500

    lazy load shopping list and auth modules

--- src/app/shopping-list/shopping-list.module.ts
+++ src/app/shopping-list/shopping-list.module.ts
@@ -7,9 +7,7 @@ import { SharedModule } from '../shared/shared.module';
 @NgModule({
   declarations: [ShoppingListComponent, ShoppingListEditComponent],
   imports: [
-    RouterModule.forChild([
-      { path: 'shopping-list', component: ShoppingListComponent },
-    ]),
+    RouterModule.forChild([{ path: '', component: ShoppingListComponent }]),
     SharedModule,
   ],
 })
